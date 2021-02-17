a1aab3f06bb4e47bd038f9ddcd14d4ab96d0d658
MaximeCervera <cervera.maxime@gmail.com>
2021-02-17 15:32:05 +0100

    Debut

--- includes/header.php
+++ includes/header.php
@@ -9,6 +9,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cocktails Machine</title>
     <link rel="stylesheet" href="app/css/style.css">
+    <link rel="stylesheet" href="app/css/login.css">
+    <link rel="stylesheet" href="app/css/soiree.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <script src="app/js/app.js"></script>
 </head>
 <body>
+    <header>
+        <div class="header-logo">
+            <img src="includes/logo.jpg" alt="Logo" width="100">
+            <h1>Coktails Machine</h1>
+        </div>
+        <nav>
+            
+        </nav>
+    </header>
---
+++ soiree.php
@@ -0,0 +1,30 @@
+<?php require_once('includes/header.php'); ?>
+    <section id="soirees">
+        <div class="section-title">
+            <h1>Choix de la soirée</h1>
+        </div>
+        <div class="soiree first">
+            <p>Ajouter une soirée</p>
+            <a href="#"><i class="fas fa-plus-circle"></i></a>
+        </div>
+        <div class="soiree">
+            <p>12/05/21</p>
+            <p>Chez Mini Trump</p>
+            <p>8 personnes</p>
+            <a href="#"> <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+        <div class="soiree">
+            <p>12/05/21</p>
+            <p>Chez Mini Trump</p>
+            <p>8 personnes</p>
+            <a href="#"> <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+        <div class="soiree">
+            <p>12/05/21</p>
+            <p>Chez Mini Trump</p>
+            <p>8 personnes</p>
+           <a href="#"> <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </section>
+<?php require_once('includes/footer.html'); ?>
+
