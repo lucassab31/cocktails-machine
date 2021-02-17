91557b3a8275c3ca4463a3bffb3acf7570ddaab9
Lucas Sabathé <luqui31@gmail.com>
2021-02-17 10:32:42 +0100

    structure

---
+++ includes/header.php
@@ -0,0 +1,14 @@
+<?php
+    require_once('app/php/bd.php');
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cocktails Machine</title>
+    <link rel="stylesheet" href="app/css/style.css">
+    <script src="app/js/app.js"></script>
+</head>
+<body>
